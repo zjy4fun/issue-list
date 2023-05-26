f04fe8ed0c33dac80d5e7fe162a73cda91515f7c
zjy4fun <zjy4fun@gmail.com>
2023-05-26 09:24:16 +0800

    feat: add current time

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const fs = require('fs');
 const path = require('path');
 const filePath = path.resolve(__dirname, 'readme.md');
 
+// 获取当前的时间
+
+const options = { year: 'numeric', month: '2-digit', day: '2-digit'};
+const time = new Date().toLocaleString('zh-CN', options);
+console.log(time);
+
 // 写入文件
-fs.writeFile(filePath, 'hello world', (err) => {
+fs.writeFile(filePath, 'hello world\n' + time, (err) => {
     if (err) {
         console.log(err);
     } else {
