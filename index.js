222c3667d48d556767f53551aeeb6ef509bc7f63
zjy4fun <zjy4fun@gmail.com>
2023-05-26 10:03:18 +0800

    feat: get issue list

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 // 读取相对路径下的readme文件，并写入hello world
 const fs = require('fs');
 const path = require('path');
+const json2md = require("json2md")
+const axios =  require('axios')
 const filePath = path.resolve(__dirname, 'readme.md');
+const {repos} = require('./repos')
 
-// 获取当前的时间
+const issues = {}
+const options = {year: 'numeric', month: '2-digit', day: '2-digit'};
+repos.forEach(async (repo) => {
+    console.log(repo)
+    try {
+        const res = await axios.get(`https://api.github.com/repos/${repo}/issues?state=open&sort=created`)
+        issues[repo] = res.data.map(issue => ({
+            title: issue.title,
+            labels: issue.labels.map(label => label.name),
+            date: new Date(issue.created_at).toLocaleDateString('zh-CN', options),
+            html_url: issue.html_url
+        }));
+    } catch (err) {
+        console.error(err)
+    }
 
-const options = { year: 'numeric', month: '2-digit', day: '2-digit'};
-const time = new Date().toLocaleString('zh-CN', options);
-console.log(time);
+})
 
-// 写入文件
-fs.writeFile(filePath, 'hello world\n\n' + time, (err) => {
+console.log(issues)
+const data = []
+for (const repo in issues) {
+    data.push({
+        h2: repo
+    })
+    data.push({
+        ul: issues[repo].map(issue => ({
+            link: {
+                title: `${issue.date} - [${issue.labels.join('] [')}] - ${issue.title}`,
+                source: issue.html_url
+            }
+        }))
+    })
+
+}
+fs.writeFile(filePath, json2md(data), (err) => {
     if (err) {
         console.log(err);
     } else {
---
+++ repos.js
@@ -0,0 +1,8 @@
+const repos = [
+    'halo-dev/halo',
+    'halo-sigs/plugin-comment-widget'
+]
+
+module.exports = {
+    repos
+}
